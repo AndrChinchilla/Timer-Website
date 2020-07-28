30be30d910fbe8de2caba20b58b5be056b17243b
Andres Chinchilla <achin059@fiu.edu>
2020-07-28 00:00:26 -0400

    Added functionality to splits

--- js/splits.js
+++ js/splits.js
@@ -3,3 +3,70 @@
  * loss or time gain, time as of split end, and the custom split time. 
  * TODO: Allow user to import splits from file. 
  */
+
+ //Validate form 
+
+
+var splits = 1   //Counts the number of splits created
+
+function validateForm()
+{
+    let elements = document.querySelector("form").elements;
+
+    var object = {};
+
+    //Validate each element of form
+    for (let element of elements){
+
+        //Validate input and save it to object
+        if (element.type == 'text' && element.value == ""){
+            
+            alert("Must be filled out");
+            return false;
+        }
+  
+
+        object[element.name] = element.value;
+        
+    }
+    //TODO: Update timer with info
+    //TODO: Allow for storage and selection of multiple timers (hash storage)
+    localStorage.setItem("timer", JSON.stringify(object));   //Save Timer to local Storage
+    
+}
+/**
+ * Add new fields to form so user can create multiple splits
+ */
+function addFields()
+{
+    splits ++; 
+
+    let container = document.getElementById("splits");
+
+    let inputName = document.createElement("input");
+    let inputTime = document.createElement("input");
+    let inputGold = document.createElement("input");
+    
+
+    inputName.type = 'text';
+    inputName.name = 'name' + splits;
+
+    inputTime.type = 'text';
+    inputTime.name = 'cumulative_time' + splits;
+
+    inputGold.type = 'text';
+    inputGold.name = 'individual_time' + splits;
+
+    container.appendChild(document.createElement("p"))
+    container.appendChild(inputName);
+    container.appendChild(inputTime);
+    container.appendChild(inputGold);
+    
+}
+
+
+
+let addButton = document.getElementById("add"); 
+
+addButton.addEventListener('click', addFields);
+
--- js/timer.js
+++ js/timer.js
@@ -13,7 +13,10 @@
 var active = false;   //Control timer
 var start = Date.now()    //Start Time
 var expected = start + 10   //Expected time for each interval
-var splits = [] ;  //Store times for all splits created
+var splits = 0 ;  //Track number of times split
+var current_splits = [] ; // Store splits created
+var gold_times = [];   //Store gold times for each variable
+
 
 var hour = 0;
 var minute = 0;
@@ -29,7 +32,7 @@ function runTimer()
 
         const millis = Date.now() - start ;  //Total milliseconds since start of timer
 
-        updateDisplay(millis);
+        updateTimer(millis);
         
         
         var drift = Date.now() - expected;
@@ -43,7 +46,7 @@ function runTimer()
 }
 
 //Update timer display with new time
-function updateDisplay(timeElapsed)
+function updateTimer(timeElapsed)
 {
           
             var updateTime = timeElapsed;
@@ -93,8 +96,6 @@ function updateDisplay(timeElapsed)
             document.getElementById("second").innerHTML = second;
             document.getElementById("hundred").innerHTML = hundred;
 
-            
-
 }
 
 //Starts timer and allows creation of splits if timer is active, resets if timer has been paused
@@ -110,10 +111,11 @@ function time()
         runTimer();
         console.log("Timer started");
         document.getElementById("control").innerHTML = "SPLIT";
-
+    
     }
     else
     {
+        
         split();
     }
 }
@@ -156,49 +158,185 @@ function reset()
         document.getElementById("control").innerHTML = "START";  
         document.getElementById("reset").innerHTML = "STOP";
 
-        //Save all stored splits to file and clear array
-        //TODO: Learn about json and file storage (Eloquent Javascript ch 20 on node.js)
+        current_splits = []
+        gold_times = []
+
+        updateDisplay();
+        splits = 0;
     }
    
 }
 
-//Takes current time and saves it (known as a split) and displays it without pausing the timer.
+//Takes current time and compares it to time of current split, updates split with the new time and time differential between splits.
 function split()
 {
-    splits.push([hour,minute,second,hundred]); 
+    var created_splits = document.getElementsByClassName("cumulative");
+
+    if (splits < created_splits.length)
+    {
+        var para = document.createElement("p");
+
+        current_split = parseInt(current_splits[splits].replace(':' , ''));
+        gold_time = parseInt(gold_times[splits].replace(':',''));
+ 
+        if (hour > 0)
+        {
+            hour = hour + ':';
+        }
+        else
+        {
+            hour = "";
+        }
+        if(minute > 0)
+        {
+            minute = minute + ':';
+        }
+        else
+        {
+            minute = "";
+
+        }
+        
+        let current_time = hour + "" + minute + "" + second + "." + hundred;
+
+    
+        created_splits[splits].innerHTML = (current_time);
+
+        
+        current_time = parseFloat(current_time.replace(':',''));   //convert time into float to perform comparison
+
+        time_differential = current_split - current_time;   //Time differential of time split compared to the split being ran against. 
+        let differential_display = createTime(time_differential.toString());
+        
+        if (time_differential > 0)
+        {
+            if (current_time > gold_time)
+            {
+                differential_display = "<span class = time_gain>" + "-" + differential_display + "</span>"
+            }
+            else
+            {
+                differential_display = "<span class = gold_time>" + "-" + differential_display + "</span>"
+            }
+            
+        }
+        else
+        {
+            differential_display = "<span class = time_loss>" + "+" + differential_display + "</span>"
+        }
+
+        created_splits[splits].insertAdjacentHTML("beforebegin", differential_display);
+
+
+        splits ++;
+        if (splits == created_splits.length)
+        {
+            document.getElementById("control").innerHTML = "STOP";
+        }
+
+    }
+    else
+    {
+        reset();
+    }
+}
+
+//Converts string into time format (ex: hour: minutes: seconds)
+function createTime(string)
+{
+    let flag = false;
+    let number_index = 0;
+
+    for (index = string.length - 1; index > 0 ; index --)
+    {   
+        if (flag)
+        {
+            if (string[index] != ':')
+            {
+                number_index ++;
+            }
+            
+            if (number_index % 2 == 0)
+            {
+                string = string.slice(0, index ) + ':' + string.slice(index );
+            }
+            
+        }
+
+        if (string[index] == '.')
+        {
+            flag = true;
+        }
+    } 
+
+    return string
+
+}
+
+
+//Updates timer with selected timer info.
+function updateDisplay()
+{
+    var object = localStorage.getItem("timer");
+    var timer = JSON.parse(object);
+
+    console.log(timer)
+    
+
     var para = document.createElement("p");
-    
+    var splits = document.getElementById("splits")
 
-    if (hour > 0)
-    {
-        hour = hour + ':';
-    }
-    else
-    {
-        hour = "";
-    }
-    if(minute > 0)
-    {
-        minute = minute + ':';
-    }
-    else
-    {
-        minute = "";
+    for(const [key, value] of Object.entries(timer)){
+        if (key == 'category')
+        {
+            document.getElementById("category").innerHTML = timer.category; 
+        }
+        
+        if (key.includes("name"))
+        {
+              
+            var name = document.createElement('span');
+            name.className = "name"
 
+            var nameContent = document.createTextNode(value);
+            name.appendChild(nameContent) 
+            para.appendChild(name);
+        
+        }
+        else if (key.includes("cumulative_time"))
+        {
+            var cumulative = document.createElement('span');
+            cumulative.className = "cumulative"
+
+            current_splits.push(value);
+
+            var cumulativeTime = document.createTextNode(value);
+            cumulative.appendChild(cumulativeTime) 
+            para.appendChild(cumulative);
+
+
+
+            splits.appendChild(para);
+            splits.append(document.createElement("hr"));
+            para = document.createElement("p");    
+        }
+        else if(key.includes("individual_time"))
+        {
+            gold_times.push(value);
+        }
+         
     }
-    var content = document.createTextNode(hour + "" + minute + "" + second + "." + hundred);
-    para.appendChild(content);
-    var element = document.getElementById("splits");
-    element.appendChild(para);
-    
+        
 }
 
 
 let startButton = document.getElementById("control");
 let resetButton = document.getElementById("reset");
+let updateDisplayButton = document.getElementById("update");
 
 
 startButton.addEventListener('click', time);
 resetButton.addEventListener('click', reset);
+updateDisplayButton.addEventListener('click', updateDisplay);
 
 
